d620f17968e45b2ea98a746f857a4e2eff086439
rizwana573 <95849132+rizwana573@users.noreply.github.com>
2023-10-17 17:45:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 // complete this js code
-function Person(name, age) {}
+function Person(name, age) {
+	this.name = name;
+	thios.age = age;
+	Person.prototype.greet(){
+		console.log(`Hello my name is ${this.name}, I am ${this.age} years old.`);
+	}
+}
 
-function Employee(name, age, jobTitle) {}
+function Employee(name, age, jobTitle) {
+	this.name = name;
+	this.age = age;
+	this.jobTitle = jobTitle;
+	 Employee.prototype = Person;
+	Employee.prototype.jobGreet(){
+		console.log(`Hello my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}`);		
+	}
+}
 
 // Do not change code below this line
 window.Person = Person;
